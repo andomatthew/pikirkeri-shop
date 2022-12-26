f2d426b6ca36910a49e9e4c51034da7243d8d133
Alexandro M <alexandro.matthew@gmail.com>
2022-12-26 15:08:16 +0700

    style: cart & wishlit page

--- src/views/CartPage/index.vue
+++ src/views/CartPage/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    Cart Page
+  <div class="px-4">
+    <img 
+      src="../../assets/illustrations/empty_cart.png"
+      alt="empty-cart-image"
+      width="216"
+      class="d-block mx-auto"
+    />
+
+    <div class="info-wrapper mx-auto">
+      <h6 class="text-h6 font-weight-bold text-center mt-4">Keranjang Kosong</h6>
+      <p class="text-caption text-center mt-4">Tambahkan produk ke keranjang sebelum kehabisan...</p>
+      <v-btn 
+        color="deep-purple"
+        variant="flat"
+        block
+        class="mt-6"
+        to="/">
+        Belanja Sekarang
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -9,5 +27,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.info-wrapper {
+  max-width: 286px;
+}
 </style>
