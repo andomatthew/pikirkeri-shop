9c903a708c69b9dff5dd73c125c75117bb478919
Alexandro M <alexandro.matthew@gmail.com>
2022-12-27 12:07:24 +0700

    feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,51 @@ export default defineConfig({
     vuetify({
       styles: {configFile: 'src/assets/scss/variables.scss'},
     }),
+    VitePWA({
+      devOptions: {
+        enabled: false,
+      },
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,ttf,txt}'],
+        cleanupOutdatedCaches: true
+      },
+      manifest: {
+        name: 'Pikirkeri Shop',
+        short_name: 'Pikirshop',
+        description: 'Pikirkeri simple online shop bro!!!',
+        theme_color: '#52a8f2',
+        background_color:'#52a8f2',
+        display: 'standalone',
+        icons: [
+          {
+            "src": "src/assets/icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "src/assets/icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "src/assets/icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "src/assets/icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
@@ -18,3 +64,4 @@ export default defineConfig({
     }
   }
 })
+
